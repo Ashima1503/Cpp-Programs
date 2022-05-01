5fb222d5d3a870568bef47a15c6b2833a267d8e1
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-05-01 12:59:38 +0530

    Adding two numbers entered by user.

---
+++ Add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first_number, second_number, Sum;
+    
+    cout<<"Enter two integers: ";
+    cin>> first_number >> second_number;
+    
+    Sum= first_number + second_number;
+    
+    cout<<first_number<<" + "<<second_number<<" = "<<Sum;
+    return 0;
+}
