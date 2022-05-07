19a8db21858d8d3fb3f1142fc5fa4749daca88fe
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-05-07 22:43:01 +0530

    Learning IF-ELSE loop concepts.

---
+++ Loop1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=9;
+    if(a==9){
+        cout<<"Nine"<<endl;
+    }
+    if(a>0){
+        cout<<"Positive"<<endl;
+    }
+    else
+    {
+        cout<<"Negative"<<endl;
+    }
+    return 0;
+}
+
