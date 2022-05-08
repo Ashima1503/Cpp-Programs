d63fd21743333a5d9b89646739fea3dbe58ce725
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-05-08 23:44:09 +0530

    Learning some Loops concepts.

---
+++ Loop4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    cin>>a;
+    
+    if(a>0){
+        cout<<"a is positive";
+    }
+    else if(a<0){
+        cout<<"a is negative";
+    }
+    else{
+        cout<<"a is 0";
+    }
+    return 0;
+}
