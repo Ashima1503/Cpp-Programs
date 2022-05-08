372813098072bcb5c39c0fa33cace4c2f00cf294
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-05-08 23:42:17 +0530

    Learning some Loops concepts.

---
+++ Loop3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 24;
+    if(a>20){
+        cout<<"A"<<"\n";
+    }
+    else if(a==24){
+        cout<<"B"<<"\n";
+    }
+    else
+    {
+        cout<<"C"<<"\n";
+    }
+    cout<<a;
+    return 0;
+}
