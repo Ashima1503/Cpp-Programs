7ac999a6f79d4826603330df8d22eef0b50be2e5
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-05-12 19:25:34 +0530

    Using continue in loop.

---
+++ Loop5_continue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=0; i<5; i++){
+        cout<<"HI"<<endl;
+        cout<<"HEY"<<endl;
+        continue;
+        
+        cout<<"Hello"<<endl;
+    }
+    return 0;
+}
